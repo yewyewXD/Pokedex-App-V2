72c5f23d4f9b1f46d2883c643009883297945d66
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-31 14:26:01 +0800

    remove warning of defining viewport

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,7 +6,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
         </Head>
         <body>
