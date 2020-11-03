2ac3a7006dc4ceeab4b63217f8f2b0a6c5ba1ee7
Yew Kang Wei <yewyew6933@gmail.com>
2020-11-03 09:59:23 +0800

    reset default font and remove roboto font

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import {
   Toolbar,
   Grid,
 } from "@material-ui/core";
-import "fontsource-roboto";
 import { PokemonContext } from "./context/PokemonState";
 import SearchBar from "./components/SearchBar";
 import OverviewCard from "./components/pokemon/OverviewCard";
@@ -62,36 +61,47 @@ export default function App({ allPokemonDetail }) {
   }, []);
 
   return (
-    <>
-      <SearchBar />
-      <FilterBar />
+    <Grid container style={{ marginTop: "60px" }}>
+      <Grid container item justify={"center"} sm={2}>
+        <SearchBar />
+        <FilterBar />
+      </Grid>
 
-      {allPokemon && (
-        <Grid container spacing={3} justify={"center"} alignItems={"center"}>
-          {allPokemon.map((pokemon) => (
-            <Grid
-              item
-              container
-              xl={2}
-              lg={3}
-              md={4}
-              sm={6}
-              xs={12}
-              justify={"center"}
-              align={"center"}
-              key={pokemon.id}
-            >
-              <OverviewCard
-                pokemonDetail={pokemon}
-                id={pokemon.id}
-                name={pokemon.name}
-                types={pokemon.types.map((type) => type.type.name)}
-                image={pokemon.image}
-              />
-            </Grid>
-          ))}
-        </Grid>
-      )}
-    </>
+      <Grid
+        container
+        item
+        justify={"center"}
+        alignItems={"center"}
+        sm={10}
+        style={{ padding: "36px 48px" }}
+      >
+        {allPokemon && (
+          <Grid container spacing={3} justify={"center"} alignItems={"center"}>
+            {allPokemon.map((pokemon) => (
+              <Grid
+                item
+                container
+                xl={2}
+                lg={3}
+                md={4}
+                sm={6}
+                xs={12}
+                justify={"center"}
+                align={"center"}
+                key={pokemon.id}
+              >
+                <OverviewCard
+                  pokemonDetail={pokemon}
+                  id={pokemon.id}
+                  name={pokemon.name}
+                  types={pokemon.types.map((type) => type.type.name)}
+                  image={pokemon.image}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Grid>
+    </Grid>
   );
 }
